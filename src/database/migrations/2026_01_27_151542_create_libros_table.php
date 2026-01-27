a81d3037cfd0b4042c99da40af90bf21800bb912
Alvaro <alvaroramirezrodriguez2003@gmail.com>
2026-01-27 16:52:01 +0100

    creacion de controladores, models y migraciones

---
+++ src/database/migrations/2026_01_27_151542_create_libros_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('libros', function (Blueprint $table) {
+            $table->id();
+            $table->string('titulo');
+            $table->string('genero');
+            $table->integer('paginas');
+            // Relación con autor
+            $table->foreignId('autor_id')->constrained('autores')->onDelete('cascade'); // Si se borra el autor, se borran sus libros
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('libros');
+    }
+};
